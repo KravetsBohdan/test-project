cab538d63d0da1bd7a8d866e740481592595ffef
KravetsBohdan <bodyakravets@gmail.com>
2023-01-14 18:06:34 +0200

    commit

--- src/main/java/com/example/testproject/entity/User.java
+++ src/main/java/com/example/testproject/entity/User.java
@@ -1,10 +1,7 @@
 package com.example.testproject.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @NoArgsConstructor
@@ -13,6 +10,7 @@ import lombok.*;
 @Setter
 @ToString
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
